1b9961c56e9b6f82659764e171eee1d3e3ec7a1f
roseanprime <roseanprime1999@gmail.com>
2023-05-04 17:35:47 +0100

    solved

--- index.js
+++ index.js
@@ -19,26 +19,48 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
+
+    
+    const myRecipe = {
+      title: "Asian Glazed Chicken Thighs",
+      level: "Amateur Chef",
+    ingredients: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu"
+    }
+
+     await Recipe.create(myRecipe)
+     console.log(myRecipe);
+
+//Iteration 3-Insert multiple recpes
+let allRecipes = await Recipe.insertMany(data)
+console.log(allRecipes);
+
+
+//Iteration 4 -Update Recipe
+let updateRecipes = await Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100});
+console.log(updateRecipes);
+
+//Iteration 5- Removr=e a recipe
+let removeRecipes = await Recipe.deleteOne({title: 'Carrot Cake'});
+console.log(removeRecipes);
+
+ mangoose.disconnect()
   } catch (error) {
     console.log(error);
   }
 };
 
 manageRecipes();
-
-//Method 2: Using .then() method
-//If you want to use this method uncomment the code below:
-
-/* mongoose
-  .connect(MONGODB_URI)
-  .then((x) => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch((error) => {
-    console.error('Error connecting to the database', error);
-  }); */
